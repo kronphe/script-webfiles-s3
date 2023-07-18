bc80829bf2ad5f69d54bb927c8aee22045dc8e9a
Roni Konda <99994074+kronphe@users.noreply.github.com>
2023-07-18 00:03:30 -0400

    Update script-s3.sh

--- script-s3.sh
+++ script-s3.sh
@@ -4,8 +4,8 @@ yum update -y
 yum install -y httpd
 cd /var/www/html
 aws s3 sync s3://kronphe-jupiter-site /var/www/html
-unzip mole.zip
-cp -r /var/www/html/mole-main/* /var/www/html
-rm -rf mole.zip mole-main
+unzip jupiter-main.zip
+cp -r /var/www/html/jupiter-main/* /var/www/html
+rm -rf jupiter-main.zip jupiter-main
 systemctl enable httpd
 systemctl start httpd
